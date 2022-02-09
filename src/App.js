839919718028d1287f34c5ca0e0f8752e0fc82bf
“Your <you@example.com>
2022-02-09 19:35:40 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+const App = () => {
+    const [arvo, setArvo] = useState(0);
+    const kasvata = () => {
+        setArvo(arvo + 1);
+    };
+    return (
+        <div className='App'>
+            <div>{arvo}</div>
+            <button onClick={kasvata}>Kasvata</button>
+        </div>
+    );
+};
 
 export default App;
